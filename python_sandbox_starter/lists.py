2bd6bf95b6aed71b80d3f8e7bcdfc4fe5db0481b
Abel-6773 <131981621+Abel-6773@users.noreply.github.com>
2023-05-12 00:27:08 +0200

    Update lists.py

--- python_sandbox_starter/lists.py
+++ python_sandbox_starter/lists.py
@@ -1,2 +1,15 @@
 # A List is a collection which is ordered and changeable. Allows duplicate members.
 # create a list 
+# Create list
+# Use a constructor
+# Get a value
+# Get the last value
+# Get length
+# Append to list (add to last)
+# Insert into position
+# Remove a specific target from list
+# Remove with pop
+# Change value
+# Reverse list
+# Sort list
+# Reverse sort
