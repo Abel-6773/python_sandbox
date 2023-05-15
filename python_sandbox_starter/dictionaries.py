942d83f72fc09110cfc69eeda4683db2f7ecaf5a
Abel-6773 <131981621+Abel-6773@users.noreply.github.com>
2023-05-15 21:19:08 +0200

    Update dictionaries.py

--- python_sandbox_starter/dictionaries.py
+++ python_sandbox_starter/dictionaries.py
@@ -1,2 +1,15 @@
 # A Dictionary is a collection which is unordered, changeable and indexed. No duplicate members.
+
+# Create dict
+# Use constructor
+# person2 = dict(first_name='Sara', last_name='Williams')
+# Get value
+# Add key/value
+# Get dict keys
+# Get dict items
+# Copy dict
+# Remove item
+# Clear
+# Get length
+# List of dict
 # Read more about dictionaries at https://docs.python.org/3/tutorial/datastructures.html#dictionaries
